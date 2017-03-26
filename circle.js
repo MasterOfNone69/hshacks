db76f203e291d1b9e5a747b06deb2204e57b2b6d
arjun333 <arjundsampath@gmail.com>
2017-03-26 09:36:31 -0700

    Did Stuff

---
+++ circle.js
@@ -0,0 +1,53 @@
+/**
+ * Updates the donut chart's percent number and the CSS positioning of the progress bar.
+ * Also allows you to set if it is a donut or pie chart
+ * @param  {string}  el      The selector for the donut to update. '#thing'
+ * @param  {number}  percent Passing in 22.3 will make the chart show 22%
+ * @param  {boolean} donut   True shows donut, false shows pie
+ */
+function updateDonutChart(el, percent, donut) {
+    if (percent > 100) {
+        percent = 100;
+    } else if (percent < 0) {
+        percent = 0;
+    }
+    var deg = Math.round(360 * (percent / 100));
+
+    if (percent > 50) {
+        $(el + ' .pie').css('clip', 'rect(auto, auto, auto, auto)');
+        $(el + ' .right-side').css('transform', 'rotate(180deg)');
+    } else {
+        $(el + ' .pie').css('clip', 'rect(0, 1em, 1em, 0.5em)');
+        $(el + ' .right-side').css('transform', 'rotate(0deg)');
+    }
+    if (donut) {
+        $(el + ' .right-side').css('border-width', '0.1em');
+        $(el + ' .left-side').css('border-width', '0.1em');
+        $(el + ' .shadow').css('border-width', '0.1em');
+    } else {
+        $(el + ' .right-side').css('border-width', '0.5em');
+        $(el + ' .left-side').css('border-width', '0.5em');
+        $(el + ' .shadow').css('border-width', '0.5em');
+    }
+    percent = parseFloat(Math.round(percent * 100) / 100).toFixed(2);
+    $(el + ' .num').text(percent);
+    $(el + ' .left-side').css('transform', 'rotate(' + deg + 'deg)');
+}
+
+$("document").ready(function() {
+    ajax();
+});
+
+function ajax() {
+    $.ajax({
+        url: "http://10.10.182.156:8080/",
+        method: "GET",
+        success: function(data) {
+            console.log(data);
+            var percent = Math.floor(data * 100);
+            updateDonutChart("#specificChart", percent / 100, true);
+            ajax();
+
+        }
+    });
+}
